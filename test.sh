862608a70a6105a986788c4048356478c43a53d8
sawp-d <70409268+sawp-d@users.noreply.github.com>
2021-01-27 10:33:39 -0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,9 @@
-readarray -t arrayVar < ./file.txt
+## declare an array variable
+declare -a arr=("node" "ubuntu:20.04")
+
+## now loop through the above array
+for i in "${arr[@]}"
+do
+   trivy image "$i"
+   # or do whatever with individual element of the array
+done
