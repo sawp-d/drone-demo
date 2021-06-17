d51f8d237940b9805575e144b40aa134c213de20
sawp-d <70409268+sawp-d@users.noreply.github.com>
2021-06-17 14:37:55 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+$GH_API=$1
 
+curl -H "Authorization: token $GH_API" -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/sawp-d/drone-demo/collaborators
 ## declare an array variable
 ##declare -a arr=("node" "ubuntu:20.04")
-mapfile -t arr < file.txt
+#mapfile -t arr < file.txt
 
 ## now loop through the above array
-for i in "${arr[@]}"
-do
-   trivy image "$i"
+#for i in "${arr[@]}"
+#do
+#   trivy image "$i"
    # or do whatever with individual element of the array
-done
+#done
